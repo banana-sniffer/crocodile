d947134e1b545a2903b82a9d4900a624dd1bb67b
William Tai <williamt@berkeley.edu>
2020-08-16 23:51:10 -0700

    Added creature movement

--- src/creature.py
+++ src/creature.py
@@ -2,13 +2,17 @@
 
 class Creature():
 
-	def __init__(self,sight=2,health=10,stamina=10,symbol=1):
+	def __init__(self,sight=2,health=10,stamina=100,symbol=1):
 		self.sight = sight
 		self.health = health
 		self.stamina = stamina
 		self.age = 0
-		self.position = [None,None]
+		self.x = 0
+		self.y = 0
+		self.prev_x = 0
+		self.prev_y = 0
 		self.symbol = symbol
+		self.actions = []
 
 	def __repr__(self):
 		return "Implementing"
@@ -16,11 +20,70 @@ class Creature():
 	def __str__(self):
 		return "Implementing"
 
+	def view_world(self, world):
+		pass
+
 	def get_symbol(self):
 		return self.symbol
 
-	def set_position(self, row, col):
-		self.position = [row,col]
+	def set_position(self, x, y):
+		self.x = x
+		self.y = y
 
-	def get_position(self):
-		return self.position
+	def get_position(self, val):
+		if val == 0:
+			return self.x
+		elif val == 1:
+			return self.y
+		else:
+			return "Invalid value"
+
+	def get_prev_position(self, val):
+		if val == 0:
+			return self.prev_x
+		elif val == 1:
+			return self.prev_y
+		else:
+			return "Invalid value"
+
+	def possible_moves(self):
+
+
+	def move(self, cmd):
+		self.prev_x = self.get_position(0)
+		self.prev_y = self.get_position(1)
+		if cmd == 1:
+			self.y += 1
+		elif cmd == 2:
+			self.x += 1
+			self.y += 1
+		elif cmd == 3:
+			self.x += 1
+		elif cmd == 4:
+			self.x += 1
+			self.y -= 1
+		elif cmd == 5:
+			self.y -= 1
+		elif cmd == 6:
+			self.x -= 1
+			self.y -= 1
+		elif cmd == 7:
+			self.x -= 1
+		elif cmd == 8:
+			self.x -= 1
+			self.y += 1
+		else:
+			print("Invalid command")
+		self.stamina -= 1
+
+
+
+
+
+
+
+
+
+
+
+
--- src/starter.py
+++ src/starter.py
@@ -1,8 +1,8 @@
 from creature import Creature
 import random as rand
 
-rows = 30
-cols = 30
+rows = 10
+cols = 10
 
 world = [["#"] * cols for _ in range(rows)]
 
@@ -13,22 +13,28 @@ def print_grid(grid):
 			stringified_row += str(r)
 		print(stringified_row)
 
-def grid_put(row, col, symbol, grid):
-	if row > rows or row < 0 or col > cols or col < 0:
+def grid_put(x, y, symbol, grid):
+	if y >= rows or y < 0 or x >= cols or x < 0:
 		print("Invalid value!")
 	else:
-		grid[rows - row - 1][col] = symbol
+		grid[rows - y - 1][x] = symbol
+
+def update_position(creature):
+	grid_put(creature.get_prev_position(0),creature.get_prev_position(1),"#",world)
+	grid_put(creature.get_position(0),creature.get_position(1),creature.get_symbol(),world)
 
 def spawn(creature, grid):
-	row_pos = rand.randint(0,rows)
-	col_pos = rand.randint(0,cols)
-	grid_put(row_pos, col_pos, creature.get_symbol(), world)
-	creature.set_position(row_pos, col_pos)
+	x = rand.randint(0,rows-1)
+	y = rand.randint(0,cols-1)
+	grid_put(x, y, creature.get_symbol(), world)
+	creature.set_position(x, y)
 
-
-
-
-print_grid(world)
 c = Creature()
 spawn(c,world)
+print(c.get_position(0),c.get_position(1))
 print_grid(world)
+for i in range(1,9):
+	c.move(i)
+	update_position(c)
+	print_grid(world)
+	print(c.get_position(0),c.get_position(1))
